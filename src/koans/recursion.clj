8d1c2dd57acb0e0e24d72adc5ce066d34e980210
Benjamin R. Haskell <clojure@benizi.com>
2012-03-14 00:03:16 -0400

    finished

--- src/koans/recursion.clj
+++ src/koans/recursion.clj
@@ -11,10 +11,16 @@
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (<= (count coll) 1)
+    coll
+    (concat (recursive-reverse (rest coll)) (list (first coll)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= n 1)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
